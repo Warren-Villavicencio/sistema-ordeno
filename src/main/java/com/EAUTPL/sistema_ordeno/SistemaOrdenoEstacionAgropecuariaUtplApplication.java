2fde8c9daa9aec43f453d4d975594aa3013f7d85
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2025-01-30 18:41:37 -0500

    Initial commit

---
+++ src/main/java/com/EAUTPL/sistema_ordeno/SistemaOrdenoEstacionAgropecuariaUtplApplication.java
@@ -0,0 +1,13 @@
+package com.EAUTPL.sistema_ordeno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaOrdenoEstacionAgropecuariaUtplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaOrdenoEstacionAgropecuariaUtplApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EAUTPL/sistema_ordeno/SistemaOrdenoEstacionAgropecuariaUtplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EAUTPL.sistema_ordeno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaOrdenoEstacionAgropecuariaUtplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
