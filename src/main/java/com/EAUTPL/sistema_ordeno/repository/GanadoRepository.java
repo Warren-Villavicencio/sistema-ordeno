a9316bad7114036262fa48f3f3b14dfb78a67f68
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2025-02-05 23:16:39 -0500

    109

--- src/main/java/com/EAUTPL/sistema_ordeno/repository/GanadoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.EAUTPL.sistema_ordeno.repository;
-
-
-import com.EAUTPL.sistema_ordeno.model.Ganado;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GanadoRepository extends JpaRepository<Ganado, Integer> { }
--- src/main/java/com/EAUTPL/sistema_ordeno/repository/OrdenioRepository.java
+++
@@ -1,6 +0,0 @@
-package com.EAUTPL.sistema_ordeno.repository;
-
-import com.EAUTPL.sistema_ordeno.model.Ordenio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrdenioRepository extends JpaRepository<Ordenio, Integer> { }
